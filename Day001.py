3c519da89e1b67cb6874282003bdbb9997b8b745
JohnDunthorne <johndunthorne1@gmail.com>
2023-11-11 12:36:09 -0600

    1

--- Day001.py
+++ Day001.py
@@ -2,3 +2,9 @@ print("hello world!")  # Print function
 print("hello \nworld")  # use \n for new line
 print("Hello" + " " + "John")  # concatenate
 print("Hello " + input("what is your name? "))  # user input
+name = "john"  # Variable
+
+# Day 1 project
+city = input("What city did you grow up in?\n")
+pet_name = input("What is the name of your pet?\n")
+print(f"The name of your band is:\n{city} {pet_name}")
