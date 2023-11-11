ba85e13fe49fa9db6eb745ce2e0affcf92e284b2
JohnDunthorne <johndunthorne1@gmail.com>
2023-11-11 11:45:04 -0600

    1

--- Day001.py
+++ Day001.py
@@ -2,3 +2,15 @@
 print("hello world!")
 
 # easy
+
+# use \n for new line
+
+print("hello \nworld")
+
+# concatenate
+
+print("Hello" + " " + "John")
+
+# user input
+
+print("Hello " + input("what is your name? "))
--- Day002.py
+++ Day002.py
@@ -1,11 +1,1 @@
-# use \n for new line
 
-print("hello \nworld")
-
-# concatenate
-
-print("Hello" + " " + "John")
-
-# user input
-
-print("Hello " + input("what is your name? "))
