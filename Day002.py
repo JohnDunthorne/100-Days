4a4d7659fd6d83c5733df834888f2ff4fe8f3d85
John Dunthorne <johndunthorne1@gmail.com>
2023-11-12 10:55:08 -0600

    2

--- Day002.py
+++ Day002.py
@@ -1,2 +1,3 @@
 # string integer boolean float < these are data types
 # int("123") str(123) float(456) float("100.5") < these are ways to type convert
+number = round(8/3, 3) # the 3 is the number of decimal places we want
