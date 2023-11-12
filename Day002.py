7fea24e5ca18bebcc49904ae58887292b722e183
John Dunthorne <johndunthorne1@gmail.com>
2023-11-12 10:31:09 -0600

    2

--- Day002.py
+++ Day002.py
@@ -1,1 +1,2 @@
-# string integer boolean float, these are data types
+# string integer boolean float < these are data types
+# int("123") str(123) float(456) float("100.5") < these are ways to type convert
