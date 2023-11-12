796b73439e1ac865b8daf2d7356a163f807e573a
John Dunthorne <johndunthorne1@gmail.com>
2023-11-12 10:59:55 -0600

    2

--- Day002.py
+++ Day002.py
@@ -1,3 +1,4 @@
 # string integer boolean float < these are data types
 # int("123") str(123) float(456) float("100.5") < these are ways to type convert
 number = round(8/3, 3) # the 3 is the number of decimal places we want
+number = (4 // 2) # // gives a floor division, which will return a rounded down int right away, not a float
