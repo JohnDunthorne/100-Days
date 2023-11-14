ca117c998033bf48fd11da755dce6a5d769b9e5d
JohnDunthorne <johndunthorne1@gmail.com>
2023-11-14 07:08:54 -0600

    3

--- Day003.py
+++ Day003.py
@@ -6,16 +6,16 @@
 # == equal to
 # != not equal to
 
-# Which year do you want to check?
-year = 2100
+# # Which year do you want to check?
+# year = 2100
 
-# 🚨 Don't change the code above 👆
+# # 🚨 Don't change the code above 👆
 
-# Write your code below this line 👇
-if (year % 4 == 0 or year % 400 == 0) and not year % 100 == 0:
-    print("Leap year")
-else:
-    print("Not leap year")
+# # Write your code below this line 👇
+# if (year % 4 == 0 or year % 400 == 0) and not year % 100 == 0:
+#     print("Leap year")
+# else:
+#     print("Not leap year")
 
 
 # print("Thank you for choosing Python Pizza Deliveries!")
@@ -28,22 +28,22 @@ else:
 # )  # Do you want extra cheese? Y or N
 # # 🚨 Don't change the code above 👆
 # # Write your code below this line 👇
-bill = 0
-if size == "S":
-    bill += 15
-elif size == "M":
-    bill += 20
-else:
-    bill += 25
-if add_pepperoni == "Y" and size == "S":
-    bill += 2
-elif add_pepperoni == "N":
-    bill += 0
-else:
-    bill += 3
-if extra_cheese == "Y":
-    bill += 1
-else:
-    bill += 0
+# bill = 0
+# if size == "S":
+#     bill += 15
+# elif size == "M":
+#     bill += 20
+# else:
+#     bill += 25
+# if add_pepperoni == "Y" and size == "S":
+#     bill += 2
+# elif add_pepperoni == "N":
+#     bill += 0
+# else:
+#     bill += 3
+# if extra_cheese == "Y":
+#     bill += 1
+# else:
+#     bill += 0
 
-print(f"Thank you for choosing Python Pizza Deliveries! Your final bill is: ${bill}.")
+# print(f"Thank you for choosing Python Pizza Deliveries! Your final bill is: ${bill}.")
