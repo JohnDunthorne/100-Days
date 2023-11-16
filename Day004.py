03207e5c369f2f50ce974cb29ef8884a2ac02ee8
JohnDunthorne <johndunthorne1@gmail.com>
2023-11-16 06:20:43 -0600

    4

--- Day004.py
+++ Day004.py
@@ -1,3 +1,11 @@
 import random
 
 # to use the random module you can use for example random.randint(1, 10) for whole numbers, random.random() for a float between 0 and 1
+
+heads_or_tails = random.randint(1, 2)
+if heads_or_tails == 1:
+    print("Heads")
+else:
+    print("Tails")
+
+    #
