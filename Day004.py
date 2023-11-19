f504de21d460cf8ecec1b10aad4196c6b2d53b74
JohnDunthorne <johndunthorne1@gmail.com>
2023-11-19 14:07:05 -0600

    4

--- Day004.py
+++ Day004.py
@@ -1,16 +1,37 @@
 import random
 
-# to use the random module you can use for example random.randint(1, 10) for whole numbers, random.random() for a float between 0 and 1
+# # to use the random module you can use for example random.randint(1, 10) for whole numbers, random.random() for a float between 0 and 1
 
-heads_or_tails = random.randint(1, 2)
-if heads_or_tails == 1:
-    print("Heads")
-else:
-    print("Tails")
+# heads_or_tails = random.randint(1, 2)
+# if heads_or_tails == 1:
+#     print("Heads")
+# else:
+#     print("Tails")
 
-# nested lists
-list1 = [1, 2, 3, 4, 5, 6, 7]
-list2 = ["a", "b", "c", "d", "e", "f", "g"]
+# # nested lists
+# list1 = [1, 2, 3, 4, 5, 6, 7]
+# list2 = ["a", "b", "c", "d", "e", "f", "g"]
 
-list3 = [list1, list2]
-print(f"{list1}\n{list2}\n{list3}")
+# list3 = [list1, list2]
+# print(f"{list1}\n{list2}\n{list3}")
+
+# rock paper scissors game
+rock = """
+     _________
+    0          0
+    0            0
+ 0                 0
+   0              0 
+     __________-"""
+paper = """
+______________
+|             |
+|             |
+|             |
+|             |
+|             |
+|             |
+_______________"""
+scissors = """
+O--/
+0--\\"""
