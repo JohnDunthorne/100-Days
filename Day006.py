72bf899d97299b00539b0e27232c4f13e0f994a2
John Dunthorne <johndunthorne1@gmail.com>
2023-11-23 14:33:38 -0600

    6

---
+++ Day006.py
@@ -0,0 +1,11 @@
+# loops
+# for number in range for item in range(3): where 3 can be a variable
+# for item in a list, for item in (variable):
+# for loops check each item in a list or range
+
+# while number > 3: while loops will continue forever as long as the requirements are True
+count = 10
+
+while count >= 0:
+    print(count)
+    count -= 1
