9c91e6a2458d95f6953b623d6ab40d7147c949e9
John Dunthorne <johndunthorne1@gmail.com>
2023-11-24 16:32:04 -0600

    8

--- Day008.py
+++ Day008.py
@@ -10,3 +10,6 @@ my_birthday = "11/17/80"
 my_age = 43
 greet(my_name, my_birthday, my_age) # when calling a function that has parameters, arguments are expected, these can use variables or hard coded data
 # note that the arguments name does not have to match the paramenter name, but whatever is in the parentheses will be used as the argument.
+
+# The above are referred to as positional arguements, as the order they are type in is important, name must be first then birthday, then age
+# is i mixed these up they would print out in the wrong order, hence positional
