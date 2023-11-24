361e1e7295f3bf95eff6f719983ceedef231636c
John Dunthorne <johndunthorne1@gmail.com>
2023-11-24 16:22:19 -0600

    8

--- Day008.py
+++ Day008.py
@@ -1,9 +1,10 @@
 
 
-def greet(name):
+def greet(name): # name is the parameter, parameters expect arguments when function is called
     print("hi")
     print(name)
     print("how can i hellp you")
 
-name = "John"
-greet(name)
+my_name = "John"
+greet(my_name) # when calling a function that has parameters, arguments are expected, these can use variables or hard coced data
+# not that the arguments name does not have to match the paramenter name, but whatever is in the parentheses will be used as the argument.
