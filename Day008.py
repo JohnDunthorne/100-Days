2754fc453c01da5be41bb8f363584a9a95e63a87
John Dunthorne <johndunthorne1@gmail.com>
2023-11-24 17:34:10 -0600

    8

--- Day008.py
+++ Day008.py
@@ -10,13 +10,13 @@ def greet(name, birthday, age): # name is the parameter, parameters expect argum
 my_name = "John"
 my_birthday = "11/17/80"
 my_age = 43
-greet(my_name, my_birthday, my_age) # when calling a function that has parameters, arguments are expected, these can use variables or hard coded data
+# greet(my_name, my_birthday, my_age) # when calling a function that has parameters, arguments are expected, these can use variables or hard coded data
 # note that the arguments name does not have to match the paramenter name, but whatever is in the parentheses will be used as the argument.
 
 # The above are referred to as positional arguements, as the order they are type in is important, name must be first then birthday, then age
 # is i mixed these up they would print out in the wrong order, hence positional
 
-greet(birthday=my_birthday, age=my_age, name=my_name) #when doing key pairs like this, avoid spaces while assigning, good practice.
+# greet(birthday=my_birthday, age=my_age, name=my_name) #when doing key pairs like this, avoid spaces while assigning, good practice.
 
 # above is the other version of arguments called keyword arguments, i take the name of the parameter, then assign the argument to it
 # i can now assign all my arguments to the parameters names in any order i wish.
