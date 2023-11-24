9d06bd8d0a425f7a63dfe05ffa826b6a36bded84
John Dunthorne <johndunthorne1@gmail.com>
2023-11-23 21:50:48 -0600

    7

--- Day007.py
+++ Day007.py
@@ -10,7 +10,37 @@ print('''
 |__|__||__|__||__|__||___,_||___|___||__|__||__|__|
       
 ''')
-word_list = ["apple", "banana", "cherry", "orange", "grape", "kiwi", "melon", "peach", "pear", "plum"]
+word_list = [
+    "abacus", "abbreviate", "aberration", "abhorrence", "abomination", "abstinence", "accelerate", "accentuate", 
+    "accommodate", "accompaniment", "acquisition", "adulteration", "adventurous", "aesthetic", "affiliation", 
+    "aggrandize", "alleviate", "amalgamation", "ambiguous", "ameliorate", "anachronism", "analogous", 
+    "anathematize", "annihilation", "antagonistic", "apocalyptic", "apprehensive", "archaeology", "articulate", 
+    "ascertain", "assimilate", "astronomical", "atmosphere", "attenuate", "authenticity", "benevolent", 
+    "biotechnology", "bureaucracy", "cacophony", "camouflage", "capitulate", "celestial", "circumference", 
+    "coalesce", "cognizant", "collaboration", "commemorate", "concatenate", "conflagration", "conglomerate", 
+    "conscientious", "consequential", "contradiction", "corroborate", "cryptocurrency", "decipher", "delectable", 
+    "denunciation", "deprecate", "derogatory", "determination", "discombobulate", "disparate", "eccentricity", 
+    "effervescent", "egalitarian", "electromagnetic", "embellish", "enervate", "enthusiastic", "ephemeral", 
+    "equivocate", "esoteric", "evanescent", "exacerbate", "exemplify", "expedite", "extrapolate", "facetious", 
+    "fastidious", "felicitous", "flabbergasted", "fluctuate", "fortuitous", "frustration", "garrulous", 
+    "genealogy", "gesticulate", "gratuitous", "gregarious", "haphazard", "harangue", "hierarchy", "homogeneous", 
+    "idiosyncrasy", "ignominious", "illustrious", "immutable", "impetuous", "implacable", "impromptu", "incongruous", 
+    "indeterminate", "indispensable", "indomitable", "inexorable", "inquisitive", "insidious", "intrepid", 
+    "intransigent", "intrepid", "introspective", "irrefutable", "juxtapose", "kaleidoscope", "labyrinthine", 
+    "lamentation", "lexicon", "libertarian", "ludicrous", "magnanimous", "malicious", "manifestation", "mellifluous", 
+    "mercurial", "metamorphosis", "meticulous", "misanthrope", "monotonous", "multifarious", "nefarious", 
+    "obfuscate", "oblivion", "omnipotent", "omniscient", "onus", "opulent", "ostentatious", "paradoxical", 
+    "penultimate", "perfidious", "perfunctory", "perspicacious", "petrify", "philanthropy", "platitude", 
+    "plethora", "pontificate", "pragmatic", "precipitous", "procrastinate", "profligate", "proliferate", 
+    "propinquity", "quandary", "querulous", "quixotic", "rampant", "recalcitrant", "reciprocal", "reconciliation", 
+    "recumbentibus", "refrigerator", "rejuvenate", "reminisce", "repertoire", "repudiate", "resilient", 
+    "resplendent", "reticent", "rhetorical", "sacrosanct", "sagacious", "salubrious", "sanguine", "sardonic", 
+    "saturnine", "serendipity", "serendipity", "sibilant", "soliloquy", "somnambulist", "spurious", "stoicism", 
+    "sycophant", "symbiotic", "taciturn", "tempestuous", "tenacious", "ubiquitous", "umbrage", "unprecedented", 
+    "usurp", "vacillate", "vagabond", "vehement", "verisimilitude", "verbose", "vexatious", "vicarious", 
+    "vindicate", "virulent", "voracious", "wanderlust", "xenophobia", "yesteryear", "zealous", "zeitgeist"
+]
+
 chosen_word = random.choice(word_list)
 display = "_" * (len(chosen_word))
 print(display)
@@ -94,7 +124,7 @@ while numberoflives > 0:
                 incorrectlyguessedletters += guess
                 numberoflives -= 1
             else:
-                print(f"you,ve already tried {guess}")
+                print(f"you've already tried {guess}")
             
         if numberoflives <= 0:
             break
