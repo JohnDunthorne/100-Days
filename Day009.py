e701df6bfa9b4f9c5fd09d00cac76c88454b118b
John Dunthorne <johndunthorne1@gmail.com>
2023-11-25 12:28:43 -0600

    9

--- Day009.py
+++ Day009.py
@@ -1,5 +1,8 @@
 # Dictionaries, they should be formatted like this:
 programming_dictionary = {
     "Bug": "An error in a program that prevents the program from running as expected.", 
-    "Function": "A piece of code that you can easily call over and over again."
+    "Function": "A piece of code that you can easily call over and over again.",
 }
+
+# This code below will print the definition of bug, you use the key to print the value it is paired with.
+print(programming_dictionary["Bug"])
