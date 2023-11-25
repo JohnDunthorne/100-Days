850ff88abf7a0baca4303f2ccf528273ed1a8684
John Dunthorne <johndunthorne1@gmail.com>
2023-11-25 12:24:26 -0600

    9

---
+++ Day009.py
@@ -0,0 +1,5 @@
+# Dictionaries, they should be formatted like this:
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.", 
+    "Function": "A piece of code that you can easily call over and over again."
+}
