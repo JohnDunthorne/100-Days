b99f579da39ff6589b5273167829db30403a4fba
John Dunthorne <johndunthorne1@gmail.com>
2023-11-24 18:52:19 -0600

    8

--- Day008.py
+++ Day008.py
@@ -47,29 +47,13 @@ def caesar(p_text, p_shift):
         shifted_word += shifted_letter
     print(shifted_word)
     
-
-def encrypt(p_text, p_shift):
-    shifted_word = ""
-    for letter in p_text:
-        index = alphabet.index(letter)
-        new_index = (index + p_shift) % 26
-        shifted_letter = alphabet[new_index]
-        shifted_word += shifted_letter
-    print(shifted_word)
-
-def decrypt(p_text, p_shift):
-    shifted_word = ""
-    for letter in p_text:
-        index = alphabet.index(letter)
-        new_index = (index - p_shift) % 26
-        shifted_letter = alphabet[new_index]
-        shifted_word += shifted_letter
-    print(shifted_word)
-
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-
-caesar(text, shift)
+while True:
+    if direction != "decode" or direction != "encode":
+        print("please type either 'encode' or 'decode'")
+    else:
+        caesar(text, shift)
