6b8bba155ff8d1b6934baa02c1b1ad07df8320f0
John Dunthorne <johndunthorne1@gmail.com>
2023-11-26 08:16:28 -0600

    9

--- Day009.py
+++ Day009.py
@@ -6,3 +6,26 @@ programming_dictionary = {
 
 # This code below will print the definition of bug, you use the key to print the value it is paired with.
 print(programming_dictionary["Bug"])
+
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+# TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
+for student in student_scores:
+    score = student_scores[student] # this is replacing 'student' with whoever its currently looping through
+    if score > 90:
+        student_grades[student] = "Outstanding"
+    elif score > 80:
+        student_grades[student] = "Exceeds Expectations"
+    elif score > 70:
+        student_grades[student] =  "Acceptable"
+    else:
+        student_grades[student] = "Fail"
+
+print(student_grades)
