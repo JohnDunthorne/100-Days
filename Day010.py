1db786bb3b5cca6c13fae5052767a4b49183bba0
John Dunthorne <johndunthorne1@gmail.com>
2023-11-26 16:15:38 -0600

    10

--- Day010.py
+++ Day010.py
@@ -45,3 +45,16 @@
 # print(days)
 
 # Calculator program
+
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
