bfbe07ff98eadb8bfaae72ee357546c2cf3c1427
John Dunthorne <johndunthorne1@gmail.com>
2023-12-02 18:25:31 -0600

    11

--- Day011.py
+++ Day011.py
@@ -1,5 +1,11 @@
 # blckjack game
+import random
 
-
-
+def dealcard():
+    card = random.choice(cards)
+    return card
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+playercard1 = dealcard()
+playercard2 = dealcard()
+print(f"your cards are [{playercard1}, {playercard2}]")
