65763587c101134253be583d64af3472aa008997
John Dunthorne <johndunthorne1@gmail.com>
2023-12-17 09:58:28 -0600

    15

--- Day014.py
+++ Day014.py
@@ -9,13 +9,14 @@ YELLOW = "\033[93m"
 BLUE = "\033[94m"
 ENDC = "\033[0m"
 
+
 def clear_terminal():
     os.system("cls" if os.name == "nt" else "clear") if os.name == "nt" else "clear"
 
 
 def random_person():
     randomindex = randint(0, len(data) - 1)
-    string = (f"{data[randomindex]['name']} is a {data[randomindex]['description']} from {data[randomindex]['country']}")
+    string = f"{data[randomindex]['name']} is a {data[randomindex]['description']} from {data[randomindex]['country']}"
     followers = data[randomindex]['follower_count']
     return string, followers
 
@@ -26,11 +27,13 @@ def guess(celeb1followers, celeb2followers):
         return True
     else:
         return False
-    
+
+
 def new_celebs():
     global celeb1, celeb1followers, celeb2, celeb2followers
     celeb1, celeb1followers = celeb2, celeb2followers
     celeb2, celeb2followers = random_person()
+
 
 def celeb_selection():
     print(BLUE + celeb1 + ENDC)
@@ -41,7 +44,8 @@ def celeb_selection():
 
     print(BLUE + celeb2 + ENDC)
     # print(celeb2followers)   
-    
+
+
 clear_terminal()
 print(Day014Art.logo)
 print("Guess who has a larger social media following!")
@@ -52,14 +56,14 @@ celeb2, celeb2followers = (random_person())
 celeb_selection()
 score = 0
 while True:
-    if guess(celeb1followers, celeb2followers) == True:
+    if guess(celeb1followers, celeb2followers):
         score += 1
         clear_terminal()
-        print(GREEN + (Day014Art.correct) + ENDC)
+        print(GREEN + Day014Art.correct + ENDC)
         print(GREEN + f"Your score is {score}" + ENDC)
         new_celebs()
         celeb_selection()
     else:
         clear_terminal()
-        print(RED + (Day014Art.wrong) + GREEN + f"\nyour final score was {score}" + ENDC)
+        print(RED + Day014Art.wrong + GREEN + f"\nyour final score was {score}" + ENDC)
         break
