fd7e9fdc17222bed7f0f445a77cb4c0a869aa17a
John Dunthorne <johndunthorne1@gmail.com>
2023-12-17 13:31:21 -0600

    15

--- Day015alt.py
+++ Day015alt.py
@@ -38,30 +38,76 @@ resources = {
 water_left = (resources['water'])
 milk_left = (resources['milk'])
 coffee_left = (resources['coffee'])
+
+continue_coffee_machine = True
 # TODO: Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
+while continue_coffee_machine:
+    while True:
+        choice = input(f"“What would you like? (espresso/latte/cappuccino):”\n").lower()
+        if choice in MENU:
+            # Check and deduct resources for water
+            if 'water' in MENU[choice]['ingredients']:
+                water_needed = MENU[choice]['ingredients']['water']
+                if water_left >= water_needed:
+                    water_left -= water_needed
+                else:
+                    print("Sorry, not enough water. Please choose another drink.")
+                    continue
 
-while True:
-    choice = input(f"“What would you like? (espresso/latte/cappuccino):”\n").lower()
+            # Check and deduct resources for milk
+            if 'milk' in MENU[choice]['ingredients']:
+                milk_needed = MENU[choice]['ingredients']['milk']
+                if milk_left >= milk_needed:
+                    milk_left -= milk_needed
+                else:
+                    print("Sorry, not enough milk. Please choose another drink.")
+                    continue
+
+            # Check and deduct resources for coffee
+            if 'coffee' in MENU[choice]['ingredients']:
+                coffee_needed = MENU[choice]['ingredients']['coffee']
+                if coffee_left >= coffee_needed:
+                    coffee_left -= coffee_needed
+                else:
+                    print("Sorry, not enough coffee. Please choose another drink.")
+                    continue
+
+            # If all ingredients are available, print a message indicating the drink is being made
+            print(f"resources deducted")
+            break
+        elif choice == 'off':
+            exit()
+        elif choice == 'report':
+            print(f"water = {water_left}\n"
+                  f"milk = {milk_left}\n"
+                  f"coffee = {coffee_left}")
+        else:
+            print("invalid input")
+
+    print(f"water = {water_left}\n"
+          f"milk = {milk_left}\n"
+          f"coffee = {coffee_left}")
     if choice in MENU:
-        drink_ingredients = MENU[choice]['ingredients']
-        for ingredient, amount in drink_ingredients.items():
-            print(ingredient, amount)
-    elif choice == 'off':
-        exit()
-    elif choice == 'report':
-        print(f"water = {water_left}\n"
-              f"milk = {milk_left}\n"
-              f"coffee = {coffee_left}")
+        print(f"You chose {choice} It costs ${MENU[choice]['cost']:.2f}")
+
+    cost = MENU[choice]['cost']
+    number_of_pennies = int(input("how many pennies will you deposit: "))
+    number_of_nickles = int(input("how many nickles will you deposit: "))
+    number_of_dimes = int(input("how many dimes will you deposit: "))
+    number_of_quarters = int(input("how many quarters will you deposit: "))
+    total_money = (number_of_pennies * 0.01) + \
+                  (number_of_nickles * 0.05) + \
+                  (number_of_dimes * 0.10) + \
+                  (number_of_quarters * 0.25)
+    print(total_money)
+    if total_money >= cost:
+        change = total_money - cost
+        print(f"Enjoy your drink your change is {change:.2f}")
+    elif total_money < cost:
+        print(f"Sorry that's not enough money, here's your ${total_money:.2f} back")
+
+    continue_making = input("would you like to make another coffee Y or N: ").lower()
+    if continue_making == 'y':
+        continue_coffee_machine = True
     else:
-        print("invalid input")
-
-
-
-# TODO: Turn off the Coffee Machine by entering “off” to the prompt
-# TODO: Print report.
-
-
-# TODO: Check resources sufficient?
-# TODO: Process coins.
-# TODO: Check transaction successful?
-# TODO: Make Coffee.
+        continue_coffee_machine = False
